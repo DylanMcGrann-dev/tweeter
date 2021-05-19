9563c2dfe010780e960ce15b9cfa1ccc5a91b124
Dylan McGrann <dylanmcgrann@Dylans-MacBook-Pro.local>
2021-05-19 13:05:27 -0600

    shadow now appears when user hovers over submitted tweets as well as bottom left icons change color to red when hovered over

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,4 +3,7 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
-
+//updates the time based on how long ago the post was made
+$(document).ready(function(){
+  $("time.timeago").timeago();
+});
