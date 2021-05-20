f6bcd619541d4500b953698643a2f487742f9463
Dylan McGrann <dylanmcgrann@Dylans-MacBook-Pro.local>
2021-05-20 15:26:13 -0600

    tweeter app is now protected against cross-site scripting

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -8,7 +8,7 @@
 $(document).ready(function(){
   // loops through tweets
   // calls createTweetElement for each tweet
-  // takes return value and appends it to the tweets container
+  // takes return value and preppends it to the tweets container
   const renderTweets = function(tweets) {
     for (const tweet of tweets) {
       let $tweet = createTweetElement(tweet);
@@ -19,30 +19,54 @@ $(document).ready(function(){
   // takes in a tweet object and is responsible for returning a tweet <article>
   const createTweetElement = function(object){
     const date = new Date(object.created_at);
+    const userText = object.content.text;
     let $tweet = $(
       `<article class="tweet">
-        <header class="tweet"><h3><img src="${object.user.avatars}">${object.user.name}</h3><h3 class="lastName">${object.user.handle}</h3></header>
-          <div class="content">${object.content.text}</div>
-        <footer class="tweet"><span class="Date tweet"><time class="timeago" datetime="${date.toISOString()}">${date.toISOString()}</time>
-        </span><span class="tweet icon"><i class="fas fa-flag"></i><i class="fas fa-retweet"></i><i class="fas fa-heart"></i></span></footer>
+        <header class="tweet">
+          <h3>
+            <img src="${object.user.avatars}">${object.user.name}
+          </h3>
+          <h3 class="lastName">
+            ${object.user.handle}
+          </h3>
+        </header>
+        <div class="content">
+          ${userText}
+        </div>
+        <footer class="tweet">
+          <span class="Date tweet">
+            <time class="timeago" datetime="${date.toISOString()}">
+              ${date.toISOString()}
+            </time>
+          </span>
+            <span class="tweet icon">
+              <i class="fas fa-flag">
+              </i>
+              <i class="fas fa-retweet">
+              </i>
+              <i class="fas fa-heart">
+              </i>
+          </span>
+        </footer>
       </article>`);
-    return $tweet; 
-  };
-  // const $tweet = createTweetElement(tweetData);
-  // renderTweets(tweetData);
-
-  //prevents default form submission behaviour.
-  //then sends a ajax post to /tweets
-  $( "#target" ).submit(function( event ) {
-    event.preventDefault();
-    let value = $("#tweet-text").val();
-    if (value.length < 1) {
-      alert("You gotta say something!");
-    } else if (value.length > 140) {
-      alert("Maybe don't say that much");
-    } else if (value.length > 0 && value.length < 141) {
-
-      $.ajax({
+      $tweet.find('div.content').text(userText);
+      return $tweet; 
+    };
+    // const $tweet = createTweetElement(tweetData);
+    // renderTweets(tweetData);
+    
+    //prevents default form submission behaviour.
+    //then sends a ajax post to /tweets
+    $( "#target" ).submit(function( event ) {
+      event.preventDefault();
+      let value = $("#tweet-text").val();
+      if (value.length < 1) {
+        document.getElementById("one").style.display = "block";
+      } else if (value.length > 140) {
+        document.getElementById("two").style.display = "block";
+      } else if (value.length > 0 && value.length < 141) {
+        
+        $.ajax({
         method: "POST",
         url: "/tweets",
         data: $( this ).serialize()
