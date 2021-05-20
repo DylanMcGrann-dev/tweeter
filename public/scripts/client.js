b0b4f85977dd551cf25d31af7a7c9768b661b991
Dylan McGrann <dylanmcgrann@Dylans-MacBook-Pro.local>
2021-05-19 19:53:11 -0600

    tweets can now be submitted via ajax requests and be seen posted immediately without refreshing the page

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -6,37 +6,13 @@
 
 
 $(document).ready(function(){
-  // $("time.timeago").timeago();
-  const tweetData = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png",
-        "handle": "@SirIsaac"
-      },
-    "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-  ];
   // loops through tweets
   // calls createTweetElement for each tweet
   // takes return value and appends it to the tweets container
   const renderTweets = function(tweets) {
     for (const tweet of tweets) {
       let $tweet = createTweetElement(tweet);
-      $('#tweets-container').append($tweet);
+      $('#tweets-container').prepend($tweet);
       $("time.timeago").timeago();
     };
   };
@@ -46,24 +22,35 @@ $(document).ready(function(){
     let $tweet = $(
       `<article class="tweet">
         <header class="tweet"><h3><img src="${object.user.avatars}">${object.user.name}</h3><h3 class="lastName">${object.user.handle}</h3></header>
-    <div class="content">${object.content.text}</div>
-    <footer class="tweet"><span class="Date tweet"><time class="timeago" datetime="${date.toISOString()}">${date.toISOString()}</time>
-    </span><span class="tweet icon"><i class="fas fa-flag"></i><i class="fas fa-retweet"></i><i class="fas fa-heart"></i></span></footer>
-    </article>`);
+          <div class="content">${object.content.text}</div>
+        <footer class="tweet"><span class="Date tweet"><time class="timeago" datetime="${date.toISOString()}">${date.toISOString()}</time>
+        </span><span class="tweet icon"><i class="fas fa-flag"></i><i class="fas fa-retweet"></i><i class="fas fa-heart"></i></span></footer>
+      </article>`);
     return $tweet; 
   };
   // const $tweet = createTweetElement(tweetData);
-  renderTweets(tweetData);
+  // renderTweets(tweetData);
 
   //prevents default form submission behaviour.
+  //then sends a ajax post to /tweets
   $( "#target" ).submit(function( event ) {
     event.preventDefault();
-  $.ajax({
-    method: "POST",
-    url: "/tweets",
-    data: $( this ).serialize()
-  });
+    $.ajax({
+      method: "POST",
+      url: "/tweets",
+      data: $( this ).serialize()
+    })
+    .then(function() {
+      loadTweets();
+    });
   });
   
+  const loadTweets = function() {
+    $.getJSON('/tweets',function(result) {
+      console.log(result);
+      renderTweets(result);
+    });
+  }
+  loadTweets();
 });
 
